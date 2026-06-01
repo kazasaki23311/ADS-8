7bac1556a7bc59994c9b10453bdeb5e66f4b6e08
kazasaki23311 <julia.nikashina@gmail.com>
2026-06-02 01:29:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,11 +1,11 @@
 // Copyright 2021 NNTU-CS
 #include "bst.h"
 
-#include <algorithm>
 #include <cctype>
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <algorithm>
 #include <vector>
 
 void makeTree(BST<std::string>& tree, const char* filename) {
